4897b867cbdf4953695fc8dda994db763fb8f1ef
Ashyum <67385193+Ashyum@users.noreply.github.com>
2021-07-13 22:05:54 +0530

    ADDED DAY16 THA

---
+++ Day16/src/components/Card.jsx
@@ -0,0 +1,13 @@
+const Card =() => {
+    return (
+        <div className="card" style={{minHeight: "100px"}}>
+            <img src="https://i.redd.it/jwegf97p46o41.jpg"
+             alt="" 
+             />
+            <h1>React Card</h1>
+            <p>This is a Meme card</p>
+        </div>
+    );
+}
+
+export default Card;
